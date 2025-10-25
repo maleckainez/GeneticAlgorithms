4953b04b594880bd18b98aa9d61015a5051f7607
Inez Małecka <maleckainez@gmail.com>
2025-10-25 17:00:56 +0200

    Finalized utils

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-def loadData(path):
+def load_data(path):
     """
      This module takes path to the file that contains data in format:
      <value> <weight>
@@ -9,27 +9,27 @@ def loadData(path):
      :rtype dict
      """
     with open(path) as f:
-        fileContent = f.read()
+        file_content = f.read()
         f.close()
-    listOfItems = fileContent.split('\n')
+    list_of_items = file_content.split('\n')
     items = {}
-    for i in range(len(listOfItems)):
-        items[i] = [int(x) for x in items[i].split()]
+    for i in range(len(list_of_items)):
+        items[i] = [int(x) for x in list_of_items[i].split(" ")]
     return items
 
 
-def createPopulation(populationSize, genomeLength):
+def create_population(population_size, genome_length):
     """
     Generates an initial binary population for Genetic Algorithm.
     Each individual is a binary genome of length `genomeLength`.
 
     Gene value 1 means the item is taken, 0 means it is not.
-     :param populationSize: number of the individuals in the population (height of the numpy matrix)
-     :type populationSize: int
-     :param genomeLength: number of genes per individual (must equal number of items)
-     :type genomeLength: int
+     :param population_size: number of the individuals in the population (height of the numpy matrix)
+     :type population_size: int
+     :param genome_length: number of genes per individual (must equal number of items)
+     :type genome_length: int
      :return: 2D array of shape (populationSize, genomeLength) with binary values in {0,1}
      :rtype numpy ndarray of ints
      """
     import numpy as np
-    return np.random.randint(2, size=(populationSize, genomeLength))
+    return np.random.randint(2, size=(population_size, genome_length))
