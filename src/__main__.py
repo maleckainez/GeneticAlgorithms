790db51067950c91643a01fda96b7ca1fe7a3ed8
Inez Małecka <maleckainez@gmail.com>
2025-11-03 18:58:14 +0100

    evolution not working

--- src/__main__.py
+++ src/__main__.py
@@ -1,26 +1,46 @@
+import sys
 import methods.utils
 import methods.fitness_score
 import methods.selection_methods
 import methods.reproduction_tools
 import os
-
+from pathlib import Path
 from src.methods.reproduction_tools import single_crossover, parent_pairing
 
-LARGE_SCALE_PATH_PREFIX = "../dane AG 2/large_scale/"
-LOW_DIMENSIONAL_PATH_PREFIX = "../dane AG 2/low-dimensional/"
-MAX_WEIGHT = 1000
 
-ITEMS_VALUE_WEIGHT = methods.utils.load_data(
-    LOW_DIMENSIONAL_PATH_PREFIX + "f3_l-d_kp_4_20"
+#############################################################################
+PROJECT_PATH = Path(__file__).parent.parent
+sys.path.insert(0, str(PROJECT_PATH / "src"))
+LARGE_SCALE_PATH = PROJECT_PATH / "dane AG 2" / "large_scale"
+LOW_DIMENSIONAL_PATH = PROJECT_PATH / "dane AG 2" / "low-dimensional"
+SEED = 2137
+MAX_WEIGHT = 2000
+CROSSOVER_PROBABILITY = 1
+MUTATION_PROBABILITY = 0.1
+ITERATIONS = 500
+POPULATION_SIZE = int(1e5)
+
+ITEMS_VALUE_WEIGHT = methods.utils.load_data(LOW_DIMENSIONAL_PATH / "f3_l-d_kp_4_20")
+methods.utils.create_population_file(
+    population_size=POPULATION_SIZE,
+    genome_length=len(ITEMS_VALUE_WEIGHT),
+    stream_batch=500,
+    SEED=SEED,
 )
-
-methods.utils.create_population_file(int(4), len(ITEMS_VALUE_WEIGHT), 100, 2137)
 fitness = methods.fitness_score.calc_fitness_score(ITEMS_VALUE_WEIGHT, MAX_WEIGHT)
-parent_pool = methods.selection_methods.fitness_proportionate_selection(
-    fitness, parent_group_size=4, seed=2137
-)
-parent_pairs = parent_pairing(parent_pool)
-single_crossover(parent_pairs, seed=2137, cross_propab=1)
+for i in range(ITERATIONS):
+    parent_pool = methods.selection_methods.fitness_proportionate_selection(
+        fitness, parent_group_size=4, seed=SEED
+    )
+    parent_pairs = parent_pairing(parent_pool)
+    single_crossover(
+        parent_pairs,
+        seed=SEED,
+        cross_propab=CROSSOVER_PROBABILITY,
+        mutation_probab=MUTATION_PROBABILITY,
+    )
+    fitness = methods.fitness_score.calc_fitness_score(ITEMS_VALUE_WEIGHT, MAX_WEIGHT)
+    print(f"iteration {i}: {fitness}")
 
 
 files = ["population.dat", "population.json"]
