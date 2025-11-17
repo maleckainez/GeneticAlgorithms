e031a6667ec5cc5b29e6d73df70926fe08dc96c5
InezMalecka <maleckainez@gmail.com>
2025-11-17 22:06:37 +0100

    added selective test of dane AG 2 data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import numpy as np
 
-
 @pytest.fixture
 def test_only_pathresolver(tmp_path, monkeypatch):
     from src.classes.PathResolver import PathResolver as PR
@@ -17,3 +16,9 @@ def test_only_pathresolver(tmp_path, monkeypatch):
 def test_only_rng():
     rng = np.random.default_rng(seed=1234)
     return rng
+
+@pytest.fixture
+def root_path():
+    from pathlib import Path
+    root_path = Path(__file__).resolve().parents[1]
+    return root_path
