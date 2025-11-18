5f1a20e54d583c7f62dd31efecf1f8a6d1e888cb
InezMalecka <maleckainez@gmail.com>
2025-11-18 22:57:27 +0100

    chore: satisfy pylint

--- tests/test_module_function_generate_population.py
+++ tests/test_module_function_generate_population.py
@@ -1,10 +1,30 @@
-from src.methods.utils import create_population_file
+# pylint: disable=missing-function-docstring
+"""Defines tests for create_population_file function
+
+This module contiains test specific only for testing function
+located in src.methods.utils used to create starting populations
+for GA algorithms. Tests below ensure that populations are created
+correctly and i/o problems won't occur during memmap creation.
+"""
 from pathlib import Path
 import json
 import numpy as np
+from src.methods.utils import create_population_file
 
 
 def _validate_population_file(pr, rng, data):
+    """Private function containing assertions to minimize code repetition.
+
+    This function calls the tested function and runs multiple assertions using
+    variables provided by callers (tests).
+
+    Args:
+        pr (src.classes.PathResolver): An initialized PathResolver instance.
+        rng (np.random.RandomState): An initialized random number generator
+            state with a predefined seed.
+        data (list[int]): List containing integer values in the form
+            [population_size, genome_length, stream_batch].
+    """
     # define variables
     temp_path = pr.get_temp_path()
     name = pr.filename_constant
@@ -29,22 +49,20 @@ def _validate_population_file(pr, rng, data):
     assert mmap_file.stat().st_size != 0
     assert json_file.stat().st_size != 0
     # load json config file
-    with open(json_file, "r") as f:
+    with open(json_file, "r", encoding="utf-8") as f:
         config = json.load(f)
     filesize = config["filesize"]
     # assert that json and real filesize matches theoretical one
     assert filesize == data[0] * data[1]
     assert filesize == mmap_file.stat().st_size
-    try:
-        mmap = np.memmap(
-            mmap_file,
-            dtype=np.uint8,
-            mode="r",
-            shape=(config["population_size"], config["genome_length"]),
-        )
-        assert mmap.shape == (data[0], data[1])
-    finally:
-        del mmap
+    mmap = np.memmap(
+        mmap_file,
+        dtype=np.uint8,
+        mode="r",
+        shape=(config["population_size"], config["genome_length"]),
+    )
+    assert mmap.shape == (data[0], data[1])
+    del mmap
 
 
 def test_batch_equal_to_len(test_only_pathresolver, test_only_rng):
