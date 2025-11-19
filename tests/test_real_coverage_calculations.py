62e61a3dc8d5aaae5600c3d634988310730aef98
InezMalecka <maleckainez@gmail.com>
2025-11-19 14:55:19 +0100

    fixed coverage config and removed od pylint config

---
+++ tests/test_real_coverage_calculations.py
@@ -0,0 +1,20 @@
+# tests/test_imports_smoke.py
+
+
+def test_all_core_modules_import():
+    # Classes
+    import src.classes.ChildrenHandler  # noqa: F401
+    import src.classes.EvolutionRunner  # noqa: F401
+    import src.classes.ExperimentConfig  # noqa: F401
+    import src.classes.OutputGenerator  # noqa: F401
+    import src.classes.PathResolver  # noqa: F401
+    import src.classes.Plotter  # noqa: F401
+    import src.classes.PopulationHandler  # noqa: F401
+    import src.classes.Reproduction  # noqa: F401
+
+    # Methods (jeśli chcesz, choć już są widoczne)
+    import src.methods.experiment_defining_tools  # noqa: F401
+    import src.methods.fitness_score  # noqa: F401
+    import src.methods.logging_library  # noqa: F401
+    import src.methods.selection_methods  # noqa: F401
+    import src.methods.utils  # noqa: F401
