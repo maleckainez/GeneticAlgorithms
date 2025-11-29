ac2194444cc7b1a8e286ebcdf633cae2d0bebca8
InezMalecka <maleckainez@gmail.com>
2025-11-29 13:18:19 +0100

    Refactor: removed unused legacy methods already refactored into library

--- src/__main__.py
+++
@@ -1,9 +0,0 @@
-"""Entry point for running the genetic algorithm with YAML configuration."""
-
-from src.classes.EvolutionRunner import EvolutionRunner
-from src.methods.utils import load_yaml_config
-
-if __name__ == "__main__":
-    config = load_yaml_config("config.yaml")
-    runner = EvolutionRunner(config)
-    runner.evolve()
--- src/methods/experiment_defining_tools.py
+++
@@ -1,4 +0,0 @@
-"""Imports methods previusly placed here for temporary path resolution."""
-
-# ruff: noqa
-from src.methods.name_generator import name_generator as create_unique_experiment_name
